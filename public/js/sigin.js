b6a6ed14cdf5ae88ae54dc420c8d94e971c9eea9
EsotericZ <cjsand03@gmail.com>
2022-02-05 22:04:54 -0700

    add structure and login/logout

---
+++ public/js/sigin.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	const emailField = $('#emailField');
+	const passwordField = $('#passwordField');
+	const signinBtn = $('#signinBtn');
+	const logoutBtn = $('#logout');
+	
+	signinBtn.on('click', async function(event) {
+		event.preventDefault();
+		await $.post('/api/users/login', {
+			email: emailField.val().trim(),
+			password: passwordField.val().trim(),
+		});
+		window.location.href = '/homepage';
+	});
+	
+	logoutBtn.on('click', async function() {
+		await $.post('/api/users/logout');
+		window.location.href = '/';
+	});
+});
