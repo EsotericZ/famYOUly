6d3724c12661bc6370e222381e818c56198cc05a
EsotericZ <cjsand03@gmail.com>
2022-02-08 14:57:16 -0700

    added child model as well as modal to add a child

--- models/index.js
+++ models/index.js
@@ -1,17 +1,26 @@
+const Child = require('./Child');
 const Family = require('./Family');
 const User = require('./User');
 const Information = require('./Information');
+
+Family.hasMany(Child, {
+	foreignKey: 'familyName',
+	onDelete: 'CASCADE',
+});
+Child.belongsTo(Family, {
+	foreignKey: 'familyName'
+});
 
 Family.hasMany(User, {
 	foreignKey: 'familyName',
 	onDelete: 'CASCADE',
 });
-
 User.belongsTo(Family, {
 	foreignKey: 'familyName'
 });
 
 module.exports = {
+    Child,
     Family,
     User,
     Information,
