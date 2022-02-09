d8e37dad52f259a285a84b87726509a56eede0cf
Kirtley Adams <hello@kirtleymichelle.com>
2022-02-08 23:20:25 -0500

    todo handlebars

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const Child = require('./Child');
 const Family = require('./Family');
 const User = require('./User');
 const Information = require('./Information');
+const Todo  = require('./Todo');
 
 Family.hasMany(Child, {
 	foreignKey: 'familyName',
@@ -19,9 +20,19 @@ User.belongsTo(Family, {
 	foreignKey: 'familyName'
 });
 
+Todo.belongsTo(User, {
+	foreignKey: 'familyName'
+});
+
+User.hasMany(Todo, {
+	foreignKey: 'familyName',
+	onDelete: 'CASCADE',
+});
+
 module.exports = {
     Child,
     Family,
     User,
     Information,
+	Todo,
 };
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,10 +4,12 @@ const userRoutes = require('./userRoutes');
 const informationRoutes = require('./informationRoutes');
 const childRoutes = require('./childRoutes');
 const familyRoutes = require('./familyRoutes');
+const todoRoutes = require('./todoRoutes');
 
 router.use('/children', childRoutes);
 router.use('/families', familyRoutes);
 router.use('/users', userRoutes);
 router.use('/info', informationRoutes);
+router.use('/homepage', todoRoutes);
 
 module.exports = router;
