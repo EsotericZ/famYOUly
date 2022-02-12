9d82c9d15ba79274c09c61265c1932e5abf540bc
kai <kai@kaiconsulting.co>
2022-02-12 12:57:36 -0700

    updated info button for info page

--- public/js/information.js
+++ public/js/information.js
@@ -3,9 +3,11 @@ $(document).ready(function () {
     if (req.session.childId) {
         console.log('success');
     }
+    let child;
     await $.get(`/api/info/${req.session.childId}`)
         .then((data) => {
             console.log(data);
+            child = data
         })
 
 
