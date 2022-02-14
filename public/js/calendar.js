01bdd2d5507ec6a08bf25524d2ca5bde5d19190c
EsotericZ <cjsand03@gmail.com>
2022-02-14 07:00:32 -0700

    calendar progress

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+    
+    let allEvents = [];
+    // $("#test").each(function() {
+    //     let rowDataArray = [];
+
+        // let actualData = $this.find('td');
+        // if (actualData.length > 0) {
+            // actualData.each(function() {
+                // rowDataArray.push($(this).text());
+            // });
+            // allEvents.push(rowDataArray);
+        // }
+    // });
+    // console.log(allEvents);
+
+    const t = document.getElementsByClassName('test1');
+    for (let i = 0; i < t.length; i++) {
+        let rowData = {
+            "title": t[i].children[0].textContent,
+            "start": t[i].children[1].textContent,
+            "end": t[i].children[2].textContent,
+            "backgroundColor": t[i].children[3].textContent,
+        };
+        allEvents.push(rowData);
+    }
+    console.log(allEvents);
+
+
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
@@ -10,67 +38,67 @@ $(document).ready(function() {
         editable: true,
         eventLimit: true, // allow "more" link when too many events
         
+        events: allEvents,
         
-        
-        events: [
-            {
-                title: 'All Day Event',
-                start: '2022-02-04',
-                backgroundColor:'red'
-            },
-            {
-                title: 'Long Event',
-                start: '2022-02-07',
-                end: '2022-02-10',
-                backgroundColor:'green'
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: '2018-03-09T16:00:00'
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: '2018-03-16T16:00:00'
-            },
-            {
-                title: 'Conference',
-                start: '2018-03-11',
-                end: '2018-03-13'
-            },
-            {
-                title: 'Class',
-                start: '2022-02-12T10:00:00',
-                end: '2022-02-12T2:00:00'
-            },
-            {
-                title: 'Lunch',
-                start: '2018-03-12T12:00:00'
-            },
-            {
-                title: 'Meeting',
-                start: '2018-03-12T14:30:00'
-            },
-            {
-                title: 'Happy Hour',
-                start: '2018-03-12T17:30:00'
+        // events: [
+        //     {
+        //         title: 'All Day Event',
+        //         start: '2022-02-04',
+        //         backgroundColor:'red'
+        //     },
+        //     {
+        //         title: 'Long Event',
+        //         start: '2022-02-07',
+        //         end: '2022-02-10',
+        //         backgroundColor:'green'
+        //     },
+        //     {
+        //         id: 999,
+        //         title: 'Repeating Event',
+        //         start: '2018-03-09T16:00:00'
+        //     },
+        //     {
+        //         id: 999,
+        //         title: 'Repeating Event',
+        //         start: '2018-03-16T16:00:00'
+        //     },
+        //     {
+        //         title: 'Conference',
+        //         start: '2018-03-11',
+        //         end: '2018-03-13'
+        //     },
+        //     {
+        //         title: 'Class',
+        //         start: '2022-02-12T10:00:00',
+        //         end: '2022-02-12T2:00:00'
+        //     },
+        //     {
+        //         title: 'Lunch',
+        //         start: '2018-03-12T12:00:00'
+        //     },
+        //     {
+        //         title: 'Meeting',
+        //         start: '2018-03-12T14:30:00'
+        //     },
+        //     {
+        //         title: 'Happy Hour',
+        //         start: '2018-03-12T17:30:00'
 
-            },
-            {
-                title: 'Dinner',
-                start: '2018-03-12T20:00:00'
-            },
-            {
-                title: 'Birthday Party',
-                start: '2018-03-13T07:00:00'
-            },
-            {
-                title: 'Click for Google',
-                url: 'http://google.com/',
-                start: '2018-03-28'
-            }
-        ],
+        //     },
+        //     {
+        //         title: 'Dinner',
+        //         start: '2018-03-12T20:00:00'
+        //     },
+        //     {
+        //         title: 'Birthday Party',
+        //         start: '2018-03-13T07:00:00'
+        //     },
+        //     {
+        //         title: 'Click for Google',
+        //         url: 'http://google.com/',
+        //         start: '2018-03-28'
+        //     }
+        // ],
         eventClick: function(calEvent, jsEvent, view, resourceObj) {
             swal({
             title: calEvent.title,
