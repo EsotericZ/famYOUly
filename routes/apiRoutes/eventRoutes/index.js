0df85dbe5571b702de9074e8a886f6c955fbdfbe
EsotericZ <cjsand03@gmail.com>
2022-02-13 17:38:18 -0700

    added events stuff

--- routes/apiRoutes/eventRoutes/index.js
+++ routes/apiRoutes/eventRoutes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const {
     createEvent,
+    updateEvent,
 } = require('../../../controllers/eventController');
 
 router.post('/newevent', createEvent);
+router.post('/updateevent', updateEvent);
 
 module.exports = router;
