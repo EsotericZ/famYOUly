6e80fdb8f77e177386c17f92a50f860c7439c605
EsotericZ <cjsand03@gmail.com>
2022-02-15 16:51:44 -0700

    pictures

--- seeds/child-seeds.js
+++ seeds/child-seeds.js
@@ -9,6 +9,7 @@ const childData = [
         pronouns: 'She/Her',
         birthday: '1986-02-05',
         familyName: 'Stark',
+        picture: 'https://vignette.wikia.nocookie.net/game-of-thrones-le-trone-de-fer/images/7/73/Sansa_Stark.png/revision/latest?cb=20170818143944&path-prefix=fr',
     },
     {
         modalId: 2,
@@ -18,6 +19,7 @@ const childData = [
         pronouns: 'They/Them',
         birthday: '1988-02-08',
         familyName: 'Stark',
+        picture: '',
     },
     {
         modalId: 3,
@@ -27,6 +29,7 @@ const childData = [
         pronouns: 'He/Him',
         birthday: '1995-04-01',
         familyName: 'Stark',
+        picture: '',
     },
 ];
 
