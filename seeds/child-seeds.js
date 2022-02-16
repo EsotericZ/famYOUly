a445591e0ea2a1dfb2df6289e6b76bc24f9cfc50
EsotericZ <cjsand03@gmail.com>
2022-02-15 21:05:53 -0700

    user levels homepage

--- seeds/child-seeds.js
+++ seeds/child-seeds.js
@@ -2,7 +2,7 @@ const { Child, Contact } = require('../models');
 
 const childData = [
     {
-        modalId: 1,
+        modalId: 100,
         firstName: 'Sansa',
         lastName: 'Stark',
         nickname: 'Queen of the North',
@@ -12,7 +12,7 @@ const childData = [
         picture: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages4.fanpop.com%2Fimage%2Fforum%2F78000%2F78091_1304243113169_full.jpg&f=1&nofb=1',
     },
     {
-        modalId: 2,
+        modalId: 102,
         firstName: 'Arya',
         lastName: 'Stark',
         nickname: 'A Girl Has No Nickname',
@@ -22,7 +22,7 @@ const childData = [
         picture: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmedia4.popsugar-assets.com%2Ffiles%2F2014%2F08%2F28%2F937%2Fn%2F3019466%2F2d8695defdbd0e64_5048185%2Fi%2FArya-Stark.jpg&f=1&nofb=1',
     },
     {
-        modalId: 3,
+        modalId: 103,
         firstName: 'Bran',
         lastName: 'Stark',
         nickname: 'Bran the Broken',
