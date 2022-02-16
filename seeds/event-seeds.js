d7938379efd9136e66877142902a1bf1a3671024
Erna Lukac <e.lukac4@gmail.com>
2022-02-15 20:34:43 -0700

    additions to events seed

--- seeds/event-seeds.js
+++ seeds/event-seeds.js
@@ -57,6 +57,48 @@ const eventData = [
         approval: true,
         familyName: 'Stark',
     },
+    {
+        modalId: 5,
+        title: 'Winter is Coming',
+        notes: 'NOT REQ',
+        start: '2019-01-01',
+        end: '2022-02-14',
+        allDay: true,
+        startTime: '',
+        endTime: '',
+        location: '', 
+        eventType: 'Required', //Optional //Request
+        approval: true,
+        familyName: 'Stark',
+    },
+    {
+        modalId: 6,
+        title: 'Pay Debts',
+        notes: 'NOT REQ',
+        start: '2022-02-21',
+        end: '2022-02-21',
+        allDay: false,
+        startTime: '09:00',
+        endTime: '10:00',
+        location: 'Everywhere', 
+        eventType: 'Required', //Optional //Request
+        approval: true,
+        familyName: 'Lannsiter',
+    },
+    {
+        modalId: 7,
+        title: 'Attend court',
+        notes: 'NOT REQ',
+        start: '2022-02-01',
+        end: '2022-02-13',
+        allDay: true,
+        startTime: '',
+        endTime: '',
+        location: '', 
+        eventType: 'Request', //Optional //Request
+        approval: false,
+        familyName: 'Lannsiter',
+    },
 ];
 
 const seedEvents = () => Event.bulkCreate(eventData);
