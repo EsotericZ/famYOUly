0c5154d9e25a98ebfbecf1186d6d95ed338406bf
EsotericZ <cjsand03@gmail.com>
2022-02-17 17:45:28 -0700

    button coolness

--- public/js/information.js
+++ public/js/information.js
@@ -104,3 +104,9 @@ $(document).ready(function () {
         window.location.reload();
     });
 });
+
+$("[data-collapse-group]").on('show.bs.collapse', function () {
+    var $this = $(this);
+    var thisCollapseAttr = $this.attr('data-collapse-group');
+    $("[data-collapse-group='" + thisCollapseAttr + "']").not($this).collapse('hide');
+});
